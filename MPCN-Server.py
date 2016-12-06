75fab39c5620308efc24d1a61db40afe498ea218
Alex Pimania <randomiseme33@gmail.com>
2016-12-06 18:04:05 +1100

    Update MPCN-Server.py

--- MPCN-Server.py
+++ MPCN-Server.py
@@ -8,11 +8,27 @@ import random
 
 PATH = "/home/pimania/MPCN"
 clients = {}
-sessions = {}
 
-def generateId():
-    return hash(time.time())
+def getClientInfo(client):
+    clientSessionSize = 0
+    clientSessionId = clients[client]["sessionId"]
+    clientsInSession = []
+    
+    for searchClient in clients:
+        if clients[searchClient]["sessionId"] == clientSessionId:
+            clientSessionSize += 1
+            clientsInSession.append(searchClient)
+            
+    return [clientSessionSize, clientSessionId, clientsInSession]
 
+def deleteUser(client):
+    clientSessionId, clientSessionSize, clientsInSession = getClientInfo(client)
+    
+    for client in clientsInSession:
+        client.write_message("
+        del clients[client]
+    
+    
 class WShandler(tornado.websocket.WebSocketHandler):
     def open(self):
         pass
@@ -24,16 +40,16 @@ class WShandler(tornado.websocket.WebSocketHandler):
         valueArg = args["value"]
         
         if functionArg == "createSession":
-            id = generateId() #TODO
+            id = generateId() 
         elif function == "joinSession":
-
+            
         elif function == "sendMessage":
             
         elif function == "placeTile":
             
     def on_close(self):
-        if len(sessions["sessionId"]) == 2:
-            del sessions["sessionId"][self]
+        deleteUser(self)
+            
             
 
 def make_app():
